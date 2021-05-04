44ef4916ef1555a20c7372f241ad81c85a31dadb
mustafa89 <mustafa.mujahid@outlook.com>
2021-05-04 10:48:53 +0200

    :wrench: fix arandr script

--- screenlayout/homesetup.sh
+++ screenlayout/homesetup.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-2 --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI-2 --mode 2560x1440 --pos 4480x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --off
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output DP-1-2 --mode 2560x1440 --pos 1920x0 --rotate normal
+xrandr --output HDMI-2 --mode 2560x1440 --pos 4480x0 --rotate normal
+xrandr --output HDMI-1 --off
+xrandr --output DP-1 --off
+xrandr --output DP-1-3 --off
+xrandr --output DP-2 --off
+xrandr --output DP-1-1 --off
